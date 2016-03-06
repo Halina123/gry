abe5122b59d1ea2e9f43d0169c9fd88993eef0d3
halina <hmd@wp.pl>
2016-03-06 10:43:47 +0100

    1 commit

---
+++ otello/content.js
@@ -0,0 +1,5 @@
+/**
+ * Created by halina on 06.03.16.
+ */
+
+module.exports = "It works from content.js.";
---
+++ otello/webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by halina on 06.03.16.
+ */
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+};
